b237d748fc0780e18fb50189e5414bd121ac57a6
AbdulBaseer1939 <ninja19391234@gmail.com>
2024-09-21 07:50:07 +0500

    Css Updated

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".nav-list ul");
+const menuItems = document.querySelectorAll(".nav-list ul li a");
+const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+document.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 250) {
+    header.style.backgroundColor = "#293230";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
